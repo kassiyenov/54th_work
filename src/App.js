fa349e53a59a2d4f19dd4c8c8e4f7681b4e51366
Vadim Glebov <vglebov@gmail.com>
2019-02-05 22:06:21 +0600

    Применили к отображению коллекции people цикл, заменили index на person.id

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import Person from './Person/Person'
 
@@ -6,29 +6,47 @@ import Person from './Person/Person'
 class App extends Component {
 
   state = {
-      people: [
-        {name: 'Dmitrii', age: 29, hobbies: 'Hobby: Video Games'},
-        {name: 'John', age: 30, hobbies: 'Likes drinking alone'}
-      ]
+    people: [
+      {id: 'asd', name: 'Dmitrii', age: 29, hobbies: 'Hobby: Video Games'},
+      {id: 'dfadfgzcx', name: 'John', age: 30, hobbies: 'Likes drinking alone'},
+      {id: '3432', name: 'Jack', age: 69, hobbies: 'Collects post stamps'},
+      {id: 'vcx', name: 'Haru-kun', age: 19, hobbies: 'Otaku desu!'}
+    ]
   };
 
-  changeName = (event) => {
-    const person = {...this.state.people[1]};
+  findIndex = (array, condition) => {
+    for (let i = 0; i < array.length; i++) {
+      if ( condition(array[i]) ) {
+        console.log(i);
+        return i;
+      }
+    }
+    return -1;
+  };
+  changeName = (event, id) => {
+    // const index = this.state.people.findIndex(person => person.id === id);
+    const index = this.findIndex(this.state.people, person => person.id === id);
+    const person = {...this.state.people[index]};
     person.name = event.target.value;
 
     const people = [...this.state.people];
-    people[1] = person;
+    people[index] = person;
 
     this.setState({people});
   };
 
   render() {
     return (
-      <Person
-        name={this.state.people[1].name}
-        age={this.state.people[1].age}
-        changeName={this.changeName}
-      />
+      <div>
+        {this.state.people.map((person) => {
+          return <Person
+            key={person.id}
+            name={person.name}
+            age={person.age}
+            changeName={event => this.changeName(event, person.id)}
+          />
+        })}
+      </div>
     );
   }
 }
